561374b59fa1cb38d98f94a0c68e1cee0df57358
hopetolight <hopetolight@outlook.com>
2019-04-01 23:55:26 +0800

    22

--- first-angular/src/app/app-routing.module.ts
+++ first-angular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewsComponent } from './components/news/news.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeadlerComponent } from './components/headler/headler.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'news', component: NewsComponent},
+  {path: 'headler', component: HeadlerComponent},
+  {path: '**', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- first-angular/src/app/components/news/news.component.ts
+++ first-angular/src/app/components/news/news.component.ts
@@ -7,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  constructor() { }
+  public  title = '我是个新闻组件';
+  public student = {name: '张三', age: 18};
+  public html = '<div>我是一个html</div>';
+  public list: number[] = [11, 22, 33, 44, 55];
+  public arr: Array<any> = ['23e', '年后', 1];
+
+  constructor() {
+
+     this.student.age = 20;
+   }
 
   ngOnInit() {
   }
